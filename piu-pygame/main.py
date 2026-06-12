f0525f354ea6b95b190083979e941a1155edfec7
greenthunder1231 <greenthunder1231.roblox@gmail.com>
2026-06-11 22:00:00 -0400

    Added lots of features and qol things

--- piu-pygame/main.py
+++ piu-pygame/main.py
@@ -1,22 +1,19 @@
 import pygame
 import sys
 from pathlib import Path
+from screeninfo import get_monitors
+from types import SimpleNamespace
 
-SCREEN_WIDTH = 1024
-SCREEN_HEIGHT = 768
+import time
 
-basic_stream = [(0, 0, 0), (0, 0.25, 1), (0, 0.5, 2), (0, 0.75, 3), (0, 1, 4), (0, 1.25, 3), (0, 1.5, 2), (0, 1.75, 1), (0, 2, 0), (0, 2.25, 1), (0, 2.5, 2), (0, 2.75, 3), (0, 3, 4), (0, 3.25, 3), (0, 3.5, 2), (0, 3.75, 1)]
+# monitorfps = 60
 
-basic_stream_mult = basic_stream * 8
+# for monitor in get_monitors():
+#     if monitor.is_primary:
+#         monitorfps = monitor.frequency
 
-extended_stream = []
-
-ct = -1
-for i in basic_stream_mult:
-    if i[1] == 0:
-        ct += 1
-    lis = (ct, i[1], i[2])
-    extended_stream.append(lis)
+WINDOW_WIDTH = 1024
+WINDOW_HEIGHT = 768
 
 def extend(stream: list, n: int) -> list:
     ct = -1
@@ -28,13 +25,6 @@ def extend(stream: list, n: int) -> list:
         lis = (ct, i[1], i[2])
         new.append(lis)
     return new
-
-streamm = [
-    (0, 0, 0), (0, 0.25, 1), (0, 0.5, 2), (0, 0.75, 3),
-    (0, 1, 4), (0, 1.25, 3), (0, 1.5, 2), (0, 1.75, 1),
-    (0, 2, 0), (0, 2.25, 3), (0, 2.5, 1), (0, 2.75, 4),
-    (0, 3, 2), (0, 3.25, 3), (0, 3.5, 1), (0, 3.75, 2)
-]
 
 testing_beatmaps = {
     "single": {
@@ -49,15 +39,15 @@ testing_beatmaps = {
             (1, 3, 1), (1, 3.25, 2), (1, 3.5, 0), (1, 3.75, 4),
             (2, 0, 0)
         ],
-        "jump": [
-            (0, i, 0) for i in range(4)
+        "sync": [
+            (0, i, 0) for i in range(32)
         ],
         "debug": [
             (0, 0, 0), (1, 0, 1), (2, 0, 2), (3, 0, 3), (4, 0, 4)
         ]
     },
     "double": {
-        "stream": [
+        "paradoxx": [
             (0, 0, 0), (0, 0.25, 2), (0, 0.5, 1), (0, 0.75, 3),
             (0, 1, 2), (0, 1.25, 4), (0, 1.5, 3), (0, 1.75, 5),
             (0, 2, 4), (0, 2.25, 6), (0, 2.5, 5), (0, 2.75, 7),
@@ -66,45 +56,54 @@ testing_beatmaps = {
             (1, 1, 6), (1, 1.25, 9), (1, 1.5, 5), (1, 1.75, 8),
             (1, 2, 9), (1, 2.25, 8), (1, 2.5, 5), (1, 2.75, 6),
             (1, 3, 5), (1, 3.25, 7), (1, 3.5, 6), (1, 3.75, 8),
-            (2, 0, 5)
+            (2, 0, 5), (2, 0.25, 8), (2, 0.5, 7), (2, 0.75, 8),
+            (2, 1, 6), (2, 1.25, 9), (2, 1.5, 5), (2, 1.75, 8),
+            (2, 2, 9), (2, 2.25, 8), (2, 2.5, 5), (2, 2.75, 6),
+            (2, 3, 5), (2, 3.25, 7), (2, 3.5, 6), (2, 3.75, 8),
+            (3, 0, 5), (3, 0.25, 7), (3, 0.5, 6), (3, 0.75, 9),
+            (3, 1, 8), (3, 1.25, 9)
         ]
     }
 }
 
-class const:
-    def __init__(self, beatmap=None):
-        # you can change these
-        self.fps = 60 # fps
-        self.startdelay = 4000 # how long it takes from program open to beatmap start
+class _settings:
+    def __init__(self):
+        # visual
+        self.startdelay = 1500 # how long it takes from open to first beat of beatmap
         self.scroll = 700 # note scroll speed
         self.autostep = True # automate beatmap
-        self.noteskin = 'default'
+        self.noteskin = '.fallback'
+        self.showsteps = False
         
-        # dont change anything after this
-        self.bpm = 220 # map bpm
-        self.double = True # if beatmap is double or not
-        if beatmap:
-            self.BEATMAP = beatmap
-        else:
-            self.BEATMAP = testing_beatmaps['double']['stream'] # the actual beatmap
+        # sound
+        self.volume = SimpleNamespace(hitsound=0.1, song=0.1)
+        self.notesounds = True
+
+class _const:
+    def __init__(self):
+        # map info
+        self.BPM = 220 # map bpm
+        self.OFFSET = -3.376177
+        self.DOUBLE = True # if beatmap is double or not
+        self.BEATMAP = testing_beatmaps['double']['paradoxx'] # the actual beatmap
         
         new = []
         for n in self.BEATMAP:
-            val = 1000 * (n[1] + 4 * n[0]) * (60 / self.bpm)
+            val = 1000 * (n[1] + 4 * n[0]) * (60 / self.BPM)
             new.append((val, n[2]))
         self.BEATMAP = new
         del(new)
-        
         self.RECEPTORY = 70
-        self.JUDGEY = 0.5 * SCREEN_HEIGHT
+        self.JUDGEY = 0.5 * WINDOW_HEIGHT
         self.KEYS = [pygame.K_a, pygame.K_s, pygame.K_d, pygame.K_f, pygame.K_g, pygame.K_h, pygame.K_j, pygame.K_k, pygame.K_l, pygame.K_SEMICOLON]
         # self.KEYS = [pygame.K_KP_1, pygame.K_KP_7, pygame.K_KP_5, pygame.K_KP_9, pygame.K_KP_3]
-        if self.double:
-            self.COLUMNX = [(SCREEN_WIDTH / 2) + 64 * (i - 4.5) for i in range(10)]
+        if self.DOUBLE:
+            self.COLUMNX = [(WINDOW_WIDTH / 2) + 64 * (i - 4.5) for i in range(10)]
         else:
-            self.COLUMNX = [(SCREEN_WIDTH / 2) + 64*(i - 2) for i in range(5)]
+            self.COLUMNX = [(WINDOW_WIDTH / 2) + 64*(i - 2) for i in range(5)]
 
-CONST = const()
+settings = _settings()
+CONST = _const()
 
 def judge(offset):
     # negative offset value = early
@@ -148,8 +147,9 @@ def extract(path, width, height):
 
 # 1. Initialize all imported pygame modules
 pygame.init()
+pygame.mixer.init()
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), vsync=1)
 pygame.display.set_caption("Stupid It Up Prime")
 
 # 3. Create a clock object to track and control frame rate
@@ -157,28 +157,50 @@ clock = pygame.time.Clock()
 
 beatmap = CONST.BEATMAP
 
-receptorpath = Path(__file__).parent / f'noteskin/{CONST.noteskin}/receptors.png'
-glowpath = Path(__file__).parent / f'noteskin/{CONST.noteskin}/glow.png'
-notepath = Path(__file__).parent / f'noteskin/{CONST.noteskin}/notes.png'
-judgepath = Path(__file__).parent / f'noteskin/{CONST.noteskin}/judgements.png'
+parentdir = Path(__file__).parent
+
+receptorpath = parentdir / f'noteskin/{settings.noteskin}/receptors.png'
+glowpath = parentdir / f'noteskin/{settings.noteskin}/glow.png'
+notepath = parentdir / f'noteskin/{settings.noteskin}/notes.png'
+judgepath = parentdir / f'noteskin/{settings.noteskin}/judgements.png'
+clappath = parentdir / f'noteskin/{settings.noteskin}/clap.ogg'
+
+song = None
+
+for ext in  ['.mp3', '.ogg']:
+    songpath = next(parentdir.rglob('*.ogg'), None)
+    if songpath:
+        song = pygame.mixer.music.load(str(songpath))
+        pygame.mixer.music.set_volume(settings.volume.song)
+        print(f'Song file found: {songpath.name}')
+        break
+if not songpath:
+    print('No song file found.')
+
+clap = None
 
 noteskinflag = False
 
 if not receptorpath.is_file():
     noteskinflag = True
-    receptorpath = Path(__file__).parent / f'noteskin/default/receptors.png'
+    receptorpath = parentdir / f'noteskin/.fallback/receptors.png'
 if not glowpath.is_file():
     noteskinflag = True
-    glowpath = Path(__file__).parent / f'noteskin/default/receptors.png'
+    glowpath = parentdir / f'noteskin/.fallback/glow.png'
 if not notepath.is_file():
     noteskinflag = True
-    notepath = Path(__file__).parent / f'noteskin/default/receptors.png'
+    notepath = parentdir / f'noteskin/.fallback/notes.png'
 if not judgepath.is_file():
     noteskinflag = True
-    judgepath = Path(__file__).parent / f'noteskin/default/receptors.png'
+    judgepath = parentdir / f'noteskin/.fallback/judgements.png'
+
+# extra things
+if clappath.is_file():
+    clap = pygame.Sound(str(clappath))
+    clap.set_volume(settings.volume.hitsound)
 
 if noteskinflag:
-    print(f'Noteskin {CONST.noteskin} not found, reverting to default')
+    print(f'Noteskin {settings.noteskin} does not contain required files, reverting to fallback')
 
 del(noteskinflag)
 
@@ -228,7 +250,7 @@ def fadecalc(t):
     if t < 0:
         return 0
     t /= 1000
-    x = max(((CONST.bpm * t) / 60) % 1, 0)
+    x = max(((CONST.BPM * t) / 60) % 1, 0)
     return (1 - x)**2
 
 def breakcombo():
@@ -259,14 +281,13 @@ class Receptor(pygame.sprite.Sprite):
             glow_overlay.set_alpha(int(255 * hit[self.i]))
             self.image.blit(glow_overlay, (0, 0))
 
-
 class Note(pygame.sprite.Sprite):
     def __init__(self, note, noteimg):
         super().__init__()
         self.note = note
         self.visible = True
         self.x = CONST.COLUMNX[self.note[1]]
-        self.y = SCREEN_HEIGHT
+        self.y = WINDOW_HEIGHT
         self.noteimg = noteimg
         # self.width = self.noteimg.get_width()
         self.height = self.noteimg.get_height()
@@ -275,11 +296,11 @@ class Note(pygame.sprite.Sprite):
     
     def update(self, time):
         timediff = self.note[0] - time
-        timediff /= 1000 / CONST.scroll
+        timediff /= 1000 / settings.scroll
         self.y = timediff + CONST.RECEPTORY
         if self.y < -self.height / 2:
             self.kill()
-        if self.y > SCREEN_HEIGHT + self.height / 2:
+        if self.y > WINDOW_HEIGHT + self.height / 2:
             self.kill()
         self.rect = self.image.get_rect(center=(self.x, self.y))
 
@@ -291,7 +312,7 @@ class Judgement(pygame.sprite.Sprite):
         self.image = self.judgement.copy().convert_alpha()
         self.width = self.judgement.get_width()
         self.height = self.judgement.get_height()
-        self.x = SCREEN_WIDTH / 2
+        self.x = WINDOW_WIDTH / 2
         self.y = CONST.JUDGEY
         self.startfade = 1000
         self.endfade = 1500
@@ -308,11 +329,37 @@ class Judgement(pygame.sprite.Sprite):
             alpha = 255 * ((dt - self.endfade) / (self.startfade - self.endfade))
             self.image.set_alpha(alpha)
 
+class ShowStep(pygame.sprite.Sprite):
+    def __init__(self, x, y, isdouble, i, normimg, glowimg):
+        super().__init__()
+        self.i = i
+        self.brightness = 1
+        self.normimg = normimg
+        self.glowimg = glowimg
+        self.image = pygame.Surface((96, 96), pygame.SRCALPHA).convert_alpha()
+        if isdouble:
+            self.x = [x - 160, x - 160, x - 96, x - 32, x - 32, x + 32, x + 32, x + 96, x + 160, x + 160][i]
+            self.y = ([y + 64, y - 64, y, y - 64, y + 64] * 2)[i]
+        else:
+            self.x = [x - 64, x - 64, x, x + 64, x + 64][i]
+            self.y = [y + 64, y - 64, y, y - 64, y + 64] * 2[i]
+        self.rect = self.image.get_rect(center=(self.x, self.y))
+        bgsurface = pygame.Surface((64, 64))
+        bgsurface.fill((47, 47, 47))
+        self.image.blit(bgsurface, (16, 16))
+        receptor = self.normimg.copy()
+        self.image.blit(receptor, (16, 16))
+    def update(self, hit):
+        if hit[self.i] > 0:
+            glow_overlay = self.glowimg.copy()
+            glow_overlay.set_alpha(int(255 * hit[self.i]))
+            self.image.blit(glow_overlay, (0, 0))
+
 monospace = pygame.font.SysFont('Monospace', 20)
 
 running = True
 hitlen = 0
-if CONST.double:
+if CONST.DOUBLE:
     hitlen = 10
 else:
     hitlen = 5
@@ -327,15 +374,17 @@ lastjudgetime = None
 judgegroup = pygame.sprite.Group()
 notegroup = pygame.sprite.Group()
 receptorgroup = pygame.sprite.Group()
+showstepgroup = pygame.sprite.Group()
 
 while running:
-    dt = clock.tick(CONST.fps) / 1000
-    rawtime = pygame.time.get_ticks() - CONST.startdelay
-    gametime = rawtime
-    rawbeat = (gametime * CONST.bpm) / 60000
+    dt = clock.tick() / 1000
+    gametime = pygame.time.get_ticks() - settings.startdelay
+    if gametime % 1000 < 10:
+        pygame.mixer.music.play(start=(126.648 + gametime / 1000))
+    rawbeat = (gametime * CONST.BPM) / 60000
     measure = rawbeat // 4
     for i in range(hitlen):
-        hit[i] = hit[i] - dt * 7
+        hit[i] = hit[i] - dt * 6
         if hit[i] < 0:
             hit[i] = 0
 
@@ -346,7 +395,7 @@ while running:
     
     lane = 10
 
-    if CONST.autostep:
+    if settings.autostep:
         for note in beatmap:
             notetime = note[0]
             offset = gametime - notetime
@@ -359,8 +408,10 @@ while running:
                 judgegroup.empty()
                 judgegroup.add(newjudge)
                 judgementlist[0] += 1
+                if settings.notesounds:
+                    clap.play()
                 beatmap.remove(note)
-    
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -369,7 +420,7 @@ while running:
                 lane = CONST.KEYS.index(event.key)
         if lane < hitlen:
             hit[lane] = 1
-            if not CONST.autostep:
+            if not settings.autostep:
                 for note in beatmap:
                     if note[1] == lane:
                         notetime = note[0]
@@ -385,6 +436,8 @@ while running:
                                 judgementlist[result] += 1
                                 if combobreak is False:
                                     combo += 1
+                            if settings.notesounds:
+                                clap.play()
                             beatmap.remove(note)
     
     if combo > maxcombo:
@@ -404,22 +457,28 @@ while running:
     if combobreak:
         breakcombo()
 
-    screen.fill((20, 20, 30))
+    screen.fill((0, 0, 0))
     
     fade = fadecalc(gametime)
     
     receptorgroup.empty()
     notegroup.empty()
+    showstepgroup.empty()
     
     for i in range(5):
         y = CONST.RECEPTORY
-        if CONST.double:
+        if CONST.DOUBLE:
             x1 = CONST.COLUMNX[i]
             x2 = CONST.COLUMNX[i + 5]
             newrecep1 = Receptor(x1, y, i, lane_normimgs[i], lane_blinkimgs[i], lane_glowimgs[i])
             newrecep2 = Receptor(x2, y, i + 5, lane_normimgs[i], lane_blinkimgs[i], lane_glowimgs[i])
             receptorgroup.add(newrecep1)
             receptorgroup.add(newrecep2)
+            if settings.showsteps:
+                newshow1 = ShowStep(WINDOW_WIDTH / 2, WINDOW_HEIGHT * 0.75, True, i, lane_normimgs[i], lane_glowimgs[i])
+                newshow2 = ShowStep(WINDOW_WIDTH / 2, WINDOW_HEIGHT * 0.75, True, i + 5, lane_normimgs[i], lane_glowimgs[i])
+                showstepgroup.add(newshow1)
+                showstepgroup.add(newshow2)
         else:
             x = CONST.COLUMNX[i]
             newrecep = Receptor(x, y, i, lane_normimgs[i], lane_blinkimgs[i], lane_glowimgs[i])
@@ -435,9 +494,20 @@ while running:
     notegroup.draw(screen)
     judgegroup.update(lastjudgetime, gametime)
     judgegroup.draw(screen)
+    showstepgroup.update(hit)
+    if settings.showsteps:
+        if CONST.DOUBLE:
+            frame = pygame.rect.Rect(0, 0, 416, 224)
+            framepadding = pygame.rect.Rect(0, 0, 384, 192)
+            frame.center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT * 0.75)
+            framepadding.center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT * 0.75)
+            pygame.draw.rect(screen, (51, 51, 51), frame)
+            pygame.draw.rect(screen, (41, 41, 41), framepadding)
+    showstepgroup.draw(screen)
     
-    pygame.draw.line(screen, (255, 255, 255), (0, CONST.RECEPTORY), (SCREEN_WIDTH, CONST.RECEPTORY))
-    pygame.draw.line(screen, (255, 255, 255), (SCREEN_WIDTH/2, 0), (SCREEN_WIDTH/2, SCREEN_HEIGHT))
+    # pygame.draw.line(screen, (255, 255, 255), (0, CONST.RECEPTORY), (WINDOW_WIDTH, CONST.RECEPTORY))
+    # pygame.draw.line(screen, (255, 255, 255), (WINDOW_WIDTH/2, 0), (WINDOW_WIDTH/2, WINDOW_HEIGHT))
+
     
     txtsurf = monospace.render(f'time: {gametime}\nbeat: {round(rawbeat % 4, 2)}\nmeas: {measure}\ncombo:{combo}', True, (255, 255, 255))
     screen.blit(txtsurf, (0, 0))
